66ff37812f7ca7cfc4cfa5c192acef46f915aa2c
aml418 <aml418@lehigh.edu>
2015-08-28 15:56:09 +0000

    m

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+//Anna Li
+//Hello World Java Program
+//
+/* first complie the program
+          java Helloworld.java
+    run the program
+        java Helloworld*/
+//
+//define a class
+public class HelloWorld {
+// add main mathod
+  public static void main(String[] args){
+      
+//print the statement with box around it
+ System.out.println("########################");
+ System.out.println("########################");
+ System.out.println("#                      #");
+ System.out.println("#  Hello World 2014!!  #");
+ System.out.println("#                      #");
+ System.out.println("########################");
+ System.out.println("########################");
+ System.out.println("test");
+ 
+ //teest/testtest
+  }
+}

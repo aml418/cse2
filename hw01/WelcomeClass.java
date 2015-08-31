f414ef52f9a9572189c6d1325cd980cd2e5c7c13
aml418 <aml418@lehigh.edu>
2015-08-31 23:26:37 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Anna Li
+//Welcome class Java
+//
+//define a class
+public class WelcomeClass {
+ 
+  public static void main(String[] args){
+      
+//print the statement with box around it
+ System.out.println("  -----------");
+ System.out.println("  | WELCOME |");
+ System.out.println("  -----------");
+ System.out.println("  ^  ^  ^  ^  ^  ^");
+ System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+ System.out.println("<-A--M--L--4--1--8->");
+ System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+ System.out.println("  v  v  v  v  v  v");
+ System.out.println("My friend and I went shopping last week.");
+ 
+ //teest/testtest
+  }
+}
